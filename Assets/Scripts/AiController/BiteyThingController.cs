7b743bc1b994f57635784899577f9962c27418c8
Paul English <zedonk1995@yahoo.com>
2023-07-04 20:03:28 +0100

    feat: imports bitething

---
+++ Assets/Scripts/AiController/BiteyThingController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class BiteyThingController : MonoBehaviour, ILandInput
+{
+    public Vector2 MoveInput { get; private set;  }
+
+    public bool JumpIsPressed { get; private set; }
+
+    Rigidbody myRigidBody = null;
+
+    GameObject player;
+    Vector3 direction;
+    Vector3 horizontalDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        direction = player.transform.position - transform.position;
+        horizontalDirection = Vector3.ProjectOnPlane(direction, Vector3.up);
+
+        this.transform.rotation= Quaternion.LookRotation(horizontalDirection);
+    }
+}
