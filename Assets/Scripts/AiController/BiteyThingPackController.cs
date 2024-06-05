d95b96629c8d4cabc4e57c5f8babceb2bf885860
unknown <zedonk1995@yahoo.com>
2024-06-05 20:03:24 +0100

    refactor: git thinks there are no changes

---
+++ Assets/Scripts/AiController/BiteyThingPackController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BiteyThingPackController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        // step 1 find other members of pack
+        // step 2 designate tasks to pack
+        // step 3 create tasks code
+    }
+}
